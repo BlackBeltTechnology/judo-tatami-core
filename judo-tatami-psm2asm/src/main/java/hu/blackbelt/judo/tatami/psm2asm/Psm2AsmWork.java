a4ce395a15846a844a9c0205d612cd863df74591
doncsoo <donat.kiss@blackbelt.hu>
2019-08-30 11:26:41 +0200

    JNG-194 Psm2AsmWork updated

--- judo-tatami-psm2asm/src/main/java/hu/blackbelt/judo/tatami/psm2asm/Psm2AsmWork.java
+++ judo-tatami-psm2asm/src/main/java/hu/blackbelt/judo/tatami/psm2asm/Psm2AsmWork.java
@@ -1,6 +1,7 @@
 package hu.blackbelt.judo.tatami.psm2asm;
 
 import hu.blackbelt.epsilon.runtime.execution.api.Log;
+import hu.blackbelt.epsilon.runtime.execution.exceptions.ScriptExecutionException;
 import hu.blackbelt.epsilon.runtime.execution.impl.Slf4jLog;
 import hu.blackbelt.judo.meta.asm.runtime.AsmModel;
 import hu.blackbelt.judo.meta.psm.runtime.PsmModel;
@@ -9,6 +10,8 @@ import hu.blackbelt.judo.tatami.core.workflow.work.TransformationContext;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
 
 import static hu.blackbelt.judo.meta.psm.PsmEpsilonValidator.validatePsm;
 import static hu.blackbelt.judo.tatami.psm2asm.Psm2Asm.executePsm2AsmTransformation;
@@ -29,26 +32,28 @@ public class Psm2AsmWork extends AbstractTransformationWork {
 
     @Override
     public void execute() throws Exception {
-        PsmModel psmModel = getTransformationContext().getByClass(PsmModel.class);
-        if (psmModel == null) {
-            throw new IllegalArgumentException("PSM Model does not found in transformation context");
-        }
+    	
+        Optional<PsmModel> psmModel = getTransformationContext().getByClass(PsmModel.class);
+        psmModel.orElseThrow(() -> new IllegalArgumentException("PSM Model does not found in transformation context"));
+        getTransformationContext().get(URI.class, PSM_VALIDATON_SCRIPT_URI)
+        	.ifPresent(validationScriptUri -> {
+        		try {
+					validatePsm(
+						getTransformationContext().getByClass(Log.class).orElseGet(() -> new Slf4jLog(log)),
+					    psmModel.get(), 
+					    validationScriptUri);
+				} catch (Exception e) {
+					e.printStackTrace();     	
+        });
 
-        if (getTransformationContext().get(PSM_VALIDATON_SCRIPT_URI) != null) {
-            validatePsm(ofNullable((Log) getTransformationContext().get(Log.class)).orElseGet(() -> new Slf4jLog(log)),
-                    psmModel, (URI) getTransformationContext().get(PSM_VALIDATON_SCRIPT_URI));
-        }
-
-        AsmModel asmModel = getTransformationContext().getByClass(AsmModel.class);
-        if (asmModel == null) {
-            asmModel = buildAsmModel().name(psmModel.getName()).build();
-            getTransformationContext().put(asmModel);
-        }
-
+        AsmModel asmModel = getTransformationContext().getByClass(AsmModel.class)
+        		.orElseGet(() -> buildAsmModel().name(psmModel.get().getName()).build());
+        getTransformationContext().put(asmModel);
+        
         Psm2AsmTransformationTrace psm2AsmTransformationTrace = executePsm2AsmTransformation(
-                psmModel,
+                psmModel.get(),
                 asmModel,
-                ofNullable((Log) getTransformationContext().get(Log.class)).orElseGet(() -> new Slf4jLog(log)),
+                getTransformationContext().getByClass(Log.class).orElseGet(() -> new Slf4jLog(log)),
                 transformationScriptRoot);
 
         getTransformationContext().put(psm2AsmTransformationTrace);
