d89ab5005ce8374a501fa9eb2232747b029f87a3
Gelei Bence <bence.gelei@blackbelt.hu>
2019-09-03 12:08:47 +0200

    JNG-194 DefaultWorkflow finished

--- judo-tatami-workflow/src/main/java/hu/blackbelt/judo/tatami/workflow/DefaultWorkflow.java
+++ judo-tatami-workflow/src/main/java/hu/blackbelt/judo/tatami/workflow/DefaultWorkflow.java
@@ -1,11 +1,16 @@
 package hu.blackbelt.judo.tatami.workflow;
 
 import hu.blackbelt.judo.meta.asm.runtime.AsmModel;
+import hu.blackbelt.judo.meta.asm.runtime.AsmModel.AsmValidationException;
 import hu.blackbelt.judo.meta.liquibase.runtime.LiquibaseModel;
+import hu.blackbelt.judo.meta.liquibase.runtime.LiquibaseModel.LiquibaseValidationException;
 import hu.blackbelt.judo.meta.measure.runtime.MeasureModel;
+import hu.blackbelt.judo.meta.measure.runtime.MeasureModel.MeasureValidationException;
 import hu.blackbelt.judo.meta.openapi.runtime.OpenapiModel;
+import hu.blackbelt.judo.meta.openapi.runtime.OpenapiModel.OpenapiValidationException;
 import hu.blackbelt.judo.meta.psm.runtime.PsmModel;
 import hu.blackbelt.judo.meta.rdbms.runtime.RdbmsModel;
+import hu.blackbelt.judo.meta.rdbms.runtime.RdbmsModel.RdbmsValidationException;
 import hu.blackbelt.judo.tatami.asm2openapi.Asm2OpenAPIWork;
 import hu.blackbelt.judo.tatami.asm2rdbms.Asm2RdbmsWork;
 import hu.blackbelt.judo.tatami.core.workflow.engine.WorkFlowEngine;
@@ -14,21 +19,21 @@ import hu.blackbelt.judo.tatami.core.workflow.flow.SequentialFlow;
 import hu.blackbelt.judo.tatami.core.workflow.flow.WorkFlow;
 import hu.blackbelt.judo.tatami.core.workflow.work.TransformationContext;
 import hu.blackbelt.judo.tatami.core.workflow.work.WorkReport;
+import hu.blackbelt.judo.tatami.core.workflow.work.WorkStatus;
 import hu.blackbelt.judo.tatami.psm2asm.Psm2AsmWork;
 import hu.blackbelt.judo.tatami.psm2measure.Psm2MeasureWork;
 import hu.blackbelt.judo.tatami.rdbms2liquibase.Rdbms2LiquibaseWork;
 
-import static hu.blackbelt.judo.meta.psm.runtime.PsmModel.loadPsmModel;
 import static hu.blackbelt.judo.tatami.core.workflow.engine.WorkFlowEngineBuilder.aNewWorkFlowEngine;
-import static hu.blackbelt.judo.meta.psm.runtime.PsmModel.LoadArguments.psmLoadArgumentsBuilder;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 
 public class DefaultWorkflow {
 	
 	TransformationContext transformationContext;
-	public static final String MODEL_NAME = "";
+	public static final String MODEL_NAME = "northwind";
     public static final String DIALECT = "hsqldb";
     public static final String MODEL_DIRECTORY = "model";
     
@@ -37,14 +42,20 @@ public class DefaultWorkflow {
     URI measureModelURI;
     URI rdbmsModelURI;
     URI liquibaseModelURI;
+    
+    private WorkReport workReport;
 	
-	public void setUp(File psmModeldest, URI asmModelURI, URI openapiModelURI, URI measureModelURI, URI rdbmsModelURI, URI liquibaseModelURI) throws IOException, PsmModel.PsmValidationException
+	public void setUp(File psmModeldest,
+					  URI asmModelURI,
+					  URI openapiModelURI,
+					  URI measureModelURI,
+					  URI rdbmsModelURI,
+					  URI liquibaseModelURI) throws IOException, PsmModel.PsmValidationException
 	{
 		transformationContext = new TransformationContext(MODEL_NAME);
-		transformationContext.put(PsmModel.buildPsmModel().name(MODEL_NAME).build());
-		/*transformationContext.put(loadPsmModel(psmLoadArgumentsBuilder()
+		transformationContext.put(PsmModel.loadPsmModel(PsmModel.LoadArguments.psmLoadArgumentsBuilder()
 				.file(psmModeldest)
-				.name(MODEL_NAME)));*/
+				.name(MODEL_NAME)));
 		transformationContext.put(Asm2RdbmsWork.RDBMS_DIALECT,DIALECT);
 		transformationContext.put(Rdbms2LiquibaseWork.LIQUIBASE_DIALECT,DIALECT);
 		transformationContext.put(Asm2RdbmsWork.RDBMS_EXCELMODEURI,new File(MODEL_DIRECTORY).toURI());
@@ -66,7 +77,7 @@ public class DefaultWorkflow {
 		Rdbms2LiquibaseWork rdbms2LiquibaseWork = new Rdbms2LiquibaseWork(transformationContext, liquibaseModelURI);
 			
 		WorkFlow workflow = SequentialFlow.Builder.aNewSequentialFlow()
-				.named("psm2asm&psm2measure, asm2dbms&measure, rdbms2liquibase")
+				.named("psm2asm&psm2measure, asm2dbms&asm2openapi, rdbms2liquibase")
 		        .execute(ParallelFlow.Builder.aNewParallelFlow()
 		        		.named("ps2asm, psm2measure")
 		        		.execute(psm2AsmWork, psm2MeasureWork)
@@ -79,7 +90,7 @@ public class DefaultWorkflow {
 		        .build();
 		
 		WorkFlowEngine workFlowEngine = aNewWorkFlowEngine().build();
-		WorkReport workReport = workFlowEngine.run(workflow);
+		workReport = workFlowEngine.run(workflow);
 		
 	     transformationContext.getByClass(AsmModel.class)
 	     	.orElseThrow(() -> new IllegalStateException("Missing transformated AsmModel"));
@@ -95,11 +106,71 @@ public class DefaultWorkflow {
 	     return workReport;
 	}
 	
-	///This Util will save out the generated model files to a defined external directory.
-	public void saveModels(URI dest)
-	{
+	public void saveModels(URI dest) 
+			throws IOException,
+				   AsmValidationException,
+				   MeasureValidationException,
+				   RdbmsValidationException,
+				   OpenapiValidationException,
+				   LiquibaseValidationException {
+		
+		if(workReport.getStatus() == WorkStatus.FAILED) {
+			throw new IllegalStateException("Transformation not yet executed");
+		}
+		
+		// AsmModel saving ///////////////
+		AsmModel asmModel = transformationContext.getByClass(AsmModel.class)
+				.orElseThrow(() -> new IllegalStateException("Missing transformated AsmModel"));
+		File asmModelDest = new File(dest.toString().replace("file:", ""),"asm.model");
+		asmModelDest.createNewFile();
+		asmModel.saveAsmModel(AsmModel.SaveArguments
+				.asmSaveArgumentsBuilder()
+				.file(asmModelDest)
+				.outputStream(new FileOutputStream(asmModelDest)));
+		
+		
+		// MeasureModel saving /////////////////
+		MeasureModel measureModel = transformationContext.getByClass(MeasureModel.class)
+				.orElseThrow(() -> new IllegalStateException("Missing transformated MeasureModel"));
+		File measureModelDest = new File(dest.toString().replace("file:", ""),"measure.model");
+		measureModelDest.createNewFile();
+		measureModel.saveMeasureModel(MeasureModel.SaveArguments
+				.measureSaveArgumentsBuilder()
+				.file(measureModelDest)
+				.outputStream(new FileOutputStream(measureModelDest)));
+				
+		
+		// RdbmsModel saving //////////////
+		RdbmsModel rdbmsModel = transformationContext.getByClass(RdbmsModel.class)
+				.orElseThrow(() -> new IllegalStateException("Missing transformated RdbmsModel"));
+		File rdbmsModelDest = new File(dest.toString().replace("file:", ""),"rdbms.model");
+		rdbmsModelDest.createNewFile();
+		rdbmsModel.saveRdbmsModel(RdbmsModel.SaveArguments
+				.rdbmsSaveArgumentsBuilder()
+				.file(rdbmsModelDest)
+				.outputStream(new FileOutputStream(rdbmsModelDest)));
+		
+		
+		// OpenapiModel saving ///////////////
+		OpenapiModel openapiModel = transformationContext.getByClass(OpenapiModel.class)
+				.orElseThrow(() -> new IllegalStateException("Missing transformated OpenapiModel"));
+		File openapiModelDest = new File(dest.toString().replace("file:", ""),"openapi.model");
+		openapiModelDest.createNewFile();
+		openapiModel.saveOpenapiModel(OpenapiModel.SaveArguments
+				.openapiSaveArgumentsBuilder()
+				.file(openapiModelDest)
+				.outputStream(new FileOutputStream(openapiModelDest)));
+
+		
+		// LiquibaseModel saving ///////////////
+		LiquibaseModel liquibaseModel = transformationContext.getByClass(LiquibaseModel.class)
+				.orElseThrow(() -> new IllegalStateException("Missing transformated LiquibaseModel"));	
+		File liquibaseModelDest = new File(dest.toString().replace("file:", ""),"liquibase.changelog.xml");
+		liquibaseModelDest.createNewFile();
+		liquibaseModel.saveLiquibaseModel(LiquibaseModel.SaveArguments
+				.liquibaseSaveArgumentsBuilder()
+				.file(liquibaseModelDest)
+				.outputStream(new FileOutputStream(liquibaseModelDest)));
 		
 	}
-	
-	
 }
--- judo-tatami-workflow/src/test/java/org/judo/tatami/workflow/DefaultWorkflowTest.java
+++ judo-tatami-workflow/src/test/java/org/judo/tatami/workflow/DefaultWorkflowTest.java
@@ -2,6 +2,7 @@ package org.judo.tatami.workflow;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,7 +11,12 @@ import java.net.URI;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import hu.blackbelt.judo.meta.asm.runtime.AsmModel.AsmValidationException;
+import hu.blackbelt.judo.meta.liquibase.runtime.LiquibaseModel.LiquibaseValidationException;
+import hu.blackbelt.judo.meta.measure.runtime.MeasureModel.MeasureValidationException;
+import hu.blackbelt.judo.meta.openapi.runtime.OpenapiModel.OpenapiValidationException;
 import hu.blackbelt.judo.meta.psm.runtime.PsmModel.PsmValidationException;
+import hu.blackbelt.judo.meta.rdbms.runtime.RdbmsModel.RdbmsValidationException;
 import hu.blackbelt.judo.tatami.core.workflow.work.WorkReport;
 import hu.blackbelt.judo.tatami.core.workflow.work.WorkStatus;
 import hu.blackbelt.judo.tatami.workflow.DefaultWorkflow;
@@ -26,15 +32,54 @@ public class DefaultWorkflowTest {
     public static final URI LIQUIBASE_URI = new File("src/main/epsilon/transformations").toURI();
     
     public static final String FILE_LOCATION = "target/test-classes/northwind-psm.model";
-	
+    
+    public static final URI TARGET_TEST_CLASSES = new File("target/test-classes").toURI();
+
+    public static final String TARGET_CLASSES = "target/test-classes";
+
+    private WorkReport workReport;
+    
+    private File asmModel;
+    private File measureModel;
+    private File rdbmsModel;
+    private File openapiModel;
+    private File liquibaseModel;
+    
 	@BeforeEach
 	void setUp() throws IOException, PsmValidationException {
+		
+		asmModel = new File(TARGET_CLASSES, "asm.model");
+		asmModel.delete();
+		measureModel = new File(TARGET_CLASSES, "measure.model");
+		measureModel.delete();
+		rdbmsModel = new File(TARGET_CLASSES, "rdbms.model");
+		rdbmsModel.delete();
+		openapiModel = new File(TARGET_CLASSES, "openapi.model");
+		openapiModel.delete();
+		liquibaseModel = new File(TARGET_CLASSES, "liquibase.changelog.xml");
+		liquibaseModel.delete();
+		
 		defaultWorkflow.setUp(new File(FILE_LOCATION),ASM_URI,OPENAPI_URI,MEASURE_URI,RDBMS_URI,LIQUIBASE_URI);
+		workReport = defaultWorkflow.startDefaultWorkflow();
+		
 	}
 	
 	@Test
-	void testDefaultWorkflow() {
-		WorkReport workReport = defaultWorkflow.startDefaultWorkflow();
+	void testDefaultWorkflow() 
+			throws IOException,
+				   AsmValidationException,
+				   MeasureValidationException,
+				   RdbmsValidationException,
+				   OpenapiValidationException,
+				   LiquibaseValidationException {
+		
 		assertThat(workReport.getStatus(), equalTo(WorkStatus.COMPLETED));
+		defaultWorkflow.saveModels(TARGET_TEST_CLASSES);
+		assertTrue(asmModel.exists());
+		assertTrue(measureModel.exists());
+		assertTrue(rdbmsModel.exists());
+		assertTrue(openapiModel.exists());
+		assertTrue(liquibaseModel.exists());
+		
 	}
 }
