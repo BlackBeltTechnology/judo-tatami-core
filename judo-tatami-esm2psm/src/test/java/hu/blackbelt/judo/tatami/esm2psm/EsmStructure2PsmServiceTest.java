41fe1170a182b5fef2c2976771e3acda69012a8b
Jozsef Borcsok <jozsef.borcsok@blackbelt.hu>
2020-01-07 20:20:48 +0100

    JNG-885: update generating getRange, set, unset, addAll and removeAll operations

--- judo-tatami-esm2psm/src/test/java/hu/blackbelt/judo/tatami/esm2psm/EsmStructure2PsmServiceTest.java
+++ judo-tatami-esm2psm/src/test/java/hu/blackbelt/judo/tatami/esm2psm/EsmStructure2PsmServiceTest.java
@@ -136,7 +136,7 @@ public class EsmStructure2PsmServiceTest {
 		
 		final Optional<hu.blackbelt.judo.meta.psm.service.BoundOperationWithRelation> psmSetOp = allPsm(
 				hu.blackbelt.judo.meta.psm.service.BoundOperationWithRelation.class)
-				.filter(o -> o.getName().equals("set_" + items.getName() + "_" + productOfItem.getName()))
+				.filter(o -> o.getName().equalsIgnoreCase("set" + productOfItem.getName() + "Via" + items.getName() + "_"))
 				.findAny();
 		
 		final Optional<hu.blackbelt.judo.meta.psm.service.TransferObjectRelation> psmProductsTransferObjectRelation = allPsm(
@@ -161,7 +161,7 @@ public class EsmStructure2PsmServiceTest {
 		
 		final Optional<hu.blackbelt.judo.meta.psm.service.BoundOperationWithRelation> psmUnsetOp = allPsm(
 				hu.blackbelt.judo.meta.psm.service.BoundOperationWithRelation.class)
-				.filter(o -> o.getName().equals("unset_" + items.getName() + "_" + productOfItem.getName()))
+				.filter(o -> o.getName().equalsIgnoreCase("unset" + productOfItem.getName() + "Via" + items.getName() + "_"))
 				.findAny();
 		
 		assertTrue(psmUnsetOp.isPresent());
