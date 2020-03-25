ff5acc8a4fe906e5004f068bdf7784522f423d66
fruska <fruzsifruzsifruzsi@gmail.com>
2020-03-25 13:50:13 +0100

    JNG-1236: psm2asm namespace tests

---
+++ judo-tatami-psm2asm/src/test/java/hu/blackbelt/judo/tatami/psm2asm/Psm2AsmNamespaceTest.java
@@ -0,0 +1,119 @@
+package hu.blackbelt.judo.tatami.psm2asm;
+
+import static hu.blackbelt.judo.meta.asm.runtime.AsmModel.buildAsmModel;
+import static hu.blackbelt.judo.meta.asm.runtime.AsmModel.SaveArguments.asmSaveArgumentsBuilder;
+import static hu.blackbelt.judo.meta.psm.PsmEpsilonValidator.calculatePsmValidationScriptURI;
+import static hu.blackbelt.judo.meta.psm.PsmEpsilonValidator.validatePsm;
+import static hu.blackbelt.judo.meta.psm.namespace.util.builder.NamespaceBuilders.newModelBuilder;
+import static hu.blackbelt.judo.meta.psm.namespace.util.builder.NamespaceBuilders.newPackageBuilder;
+import static hu.blackbelt.judo.meta.psm.runtime.PsmModel.buildPsmModel;
+import static hu.blackbelt.judo.tatami.psm2asm.Psm2Asm.calculatePsm2AsmTransformationScriptURI;
+import static hu.blackbelt.judo.tatami.psm2asm.Psm2Asm.executePsm2AsmTransformation;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.util.Optional;
+
+import org.eclipse.emf.ecore.EPackage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.ImmutableList;
+
+import hu.blackbelt.epsilon.runtime.execution.api.Log;
+import hu.blackbelt.epsilon.runtime.execution.impl.Slf4jLog;
+import hu.blackbelt.judo.meta.asm.runtime.AsmModel;
+import hu.blackbelt.judo.meta.asm.runtime.AsmUtils;
+import hu.blackbelt.judo.meta.psm.namespace.Model;
+import hu.blackbelt.judo.meta.psm.namespace.Package;
+import hu.blackbelt.judo.meta.psm.runtime.PsmModel;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Psm2AsmNamespaceTest {
+
+    public static final String MODEL_NAME = "Test";
+    public static final String TARGET_TEST_CLASSES = "target/test-classes";
+    
+    public static final String ACCESSPOINT_SOURCE = AsmUtils.getAnnotationUri("accessPoint");
+    public static final String EXPOSED_SERVICE_SOURCE = AsmUtils.getAnnotationUri("exposedService");
+    public static final String EXPOSED_GRAPH_SOURCE = AsmUtils.getAnnotationUri("exposedGraph");
+    public static final String EXPRESSION_SOURCE = AsmUtils.getAnnotationUri("expression");
+
+    public static final String NS_URI = "http://blackbelt.hu/judo/" + MODEL_NAME;
+    public static final String NS_PREFIX = "runtime" + MODEL_NAME;
+    public static final String EXTENDED_METADATA_URI = "http://blackbelt.hu/judo/meta/ExtendedMetadata";
+    
+    public static final String TEST_MODEL_NAME = "Model";
+    
+    Log slf4jlog;
+    PsmModel psmModel;
+    AsmModel asmModel;
+    AsmUtils asmUtils;
+
+    @BeforeEach
+    public void setUp() {
+        // Default logger
+        slf4jlog = new Slf4jLog(log);
+        // Loading PSM to isolated ResourceSet, because in Tatami
+        // there is no new namespace registration made.
+        psmModel = buildPsmModel()
+                .name(MODEL_NAME)
+                .build();
+        // Create empty ASM model
+        asmModel = buildAsmModel()
+                .name(MODEL_NAME)
+                .build();
+        asmUtils = new AsmUtils(asmModel.getResourceSet());
+    }
+
+    private void transform(final String testName) throws Exception {
+        psmModel.savePsmModel(PsmModel.SaveArguments.psmSaveArgumentsBuilder()
+                .file(new File(TARGET_TEST_CLASSES, getClass().getName() + "-" + testName + "-psm.model"))
+                .build());
+        
+        assertTrue(psmModel.isValid());
+        validatePsm(new Slf4jLog(log), psmModel, calculatePsmValidationScriptURI());
+
+        executePsm2AsmTransformation(psmModel, asmModel, new Slf4jLog(log), calculatePsm2AsmTransformationScriptURI());
+
+        assertTrue(asmModel.isValid());
+        asmModel.saveAsmModel(asmSaveArgumentsBuilder()
+                .file(new File(TARGET_TEST_CLASSES, getClass().getName() + "-" + testName + "-asm.model"))
+                .build());
+    }
+    
+    @Test
+    void testAccessPoint() throws Exception {
+        
+    	Package packOfPack = newPackageBuilder().withName("packageB").build();
+    	
+		Package packOfModel = newPackageBuilder().withPackages(packOfPack).withName("packageA").build();
+
+		Model model = newModelBuilder().withName(TEST_MODEL_NAME).withPackages(packOfModel)
+				.withElements(ImmutableList.of()).build();
+
+        psmModel.addContent(model);
+
+        transform("testNamespace");
+        
+        final String packageANameFirstUpperCase = "PackageA";
+        final String packageBNameFirstUpperCase = "PackageB";
+        
+        final Optional<EPackage> asmPackOfPack = asmUtils.all(EPackage.class).filter(c -> c.getName().equals(packOfPack.getName())).findAny();
+        final Optional<EPackage> asmPackOfModel = asmUtils.all(EPackage.class).filter(c -> c.getName().equals(packOfModel.getName())).findAny();
+        final Optional<EPackage> asmModel = asmUtils.all(EPackage.class).filter(c -> c.getName().equals(model.getName())).findAny();
+        assertTrue(asmPackOfPack.isPresent());
+        assertTrue(asmPackOfPack.get().getNsPrefix().equals(NS_PREFIX + TEST_MODEL_NAME + packageANameFirstUpperCase + packageBNameFirstUpperCase));
+        assertTrue(asmPackOfPack.get().getNsURI().equals(NS_URI + "/" + TEST_MODEL_NAME + "/" + packOfModel.getName() + "/" + packOfPack.getName()));
+        
+        assertTrue(asmPackOfModel.isPresent());
+        assertTrue(asmPackOfModel.get().getNsPrefix().equals(NS_PREFIX + TEST_MODEL_NAME + packageANameFirstUpperCase));
+        assertTrue(asmPackOfModel.get().getNsURI().equals(NS_URI + "/" + TEST_MODEL_NAME + "/" + packOfModel.getName()));
+        
+        assertTrue(asmModel.isPresent());
+        assertTrue(asmModel.get().getNsPrefix().equals(NS_PREFIX + TEST_MODEL_NAME));
+        assertTrue(asmModel.get().getNsURI().equals(NS_URI + "/" + TEST_MODEL_NAME));
+      
+    }
+}
