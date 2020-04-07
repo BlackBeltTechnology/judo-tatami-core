8b43b6f5a275bf47dbc177be43c7eed36e385165
Gelei Bence <bence.gelei@blackbelt.hu>
2020-04-07 14:59:39 +0200

    JNG-1241 testTwoWayRelationWithNullToOneCardinalities added but transformation is invalid

--- judo-tatami-asm2rdbms/src/test/java/hu/blackbelt/judo/tatami/asm2rdbms/Asm2RdbmsRelationMappingTest.java
+++ judo-tatami-asm2rdbms/src/test/java/hu/blackbelt/judo/tatami/asm2rdbms/Asm2RdbmsRelationMappingTest.java
@@ -1,12 +1,10 @@
 package hu.blackbelt.judo.tatami.asm2rdbms;
 
 import com.google.common.collect.ImmutableList;
-import hu.blackbelt.judo.meta.rdbms.RdbmsForeignKey;
-import hu.blackbelt.judo.meta.rdbms.RdbmsIdentifierField;
 import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.EReference;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,10 +13,12 @@ import static org.eclipse.emf.ecore.util.builder.EcoreBuilders.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Asm2RdbmsRelationMappingTest extends Asm2RdbmsMappingTestBase {
+    //////////////////////////////////////////////////
+    /////////////// ONE WAY RELATIONS ////////////////
 
     @Test
     @DisplayName("Test OneWayRelation With Null To One Cardinality")
-    public void testOneWayRelationWithNullToOneCardinality() throws Exception {
+    public void testOneWayRelationWithNullToOneCardinality() {
         // create annotation
         EAnnotation eAnnotation = newEAnnotationBuilder()
                 .withSource("http://blackbelt.hu/judo/meta/ExtendedMetadata/entity")
@@ -71,18 +71,20 @@ public class Asm2RdbmsRelationMappingTest extends Asm2RdbmsMappingTestBase {
 
         // check if tables exist
         assertEquals(2, rdbmsUtils.getRdbmsTables()
-                .orElseThrow(() -> new Exception("No tables were found")).size());
+                .orElseThrow(() -> new RuntimeException("No tables were found")).size());
 
-        assertTrue(rdbmsUtils.getRdbmsTable(RDBMS_TABLE_NAME_TO).isPresent());
-        assertTrue(rdbmsUtils.getRdbmsTable(RDBMS_TABLE_NAME_FROM).isPresent());
-
-        // check if RdbmsForeignKey exists
-        assertTrue(rdbmsUtils.getRdbmsForeignKey(RDBMS_TABLE_NAME_FROM, ONE_WAY_REFERENCE, false).isPresent());
+        // check if RdbmsForeignKey is valid
+        assertEquals(rdbmsUtils.getRdbmsTable(RDBMS_TABLE_NAME_TO)
+                        .orElseThrow(() -> new RuntimeException(RDBMS_TABLE_NAME_TO + " table not found"))
+                        .getPrimaryKey(),
+                rdbmsUtils.getRdbmsForeignKey(RDBMS_TABLE_NAME_FROM, ONE_WAY_REFERENCE, false)
+                        .orElseThrow(() -> new RuntimeException(ONE_WAY_REFERENCE + " attribute not found"))
+                        .getReferenceKey());
     }
 
     @Test
     @DisplayName("Test OneWayRelation With Null To Infinite Cardinality")
-    public void testOneWayRelationWithNullToInfiniteCardinality()  throws Exception {
+    public void testOneWayRelationWithNullToInfiniteCardinality() {
         // create annotation
         EAnnotation eAnnotation = newEAnnotationBuilder()
                 .withSource("http://blackbelt.hu/judo/meta/ExtendedMetadata/entity")
@@ -135,20 +137,186 @@ public class Asm2RdbmsRelationMappingTest extends Asm2RdbmsMappingTestBase {
 
         // check if tables exist
         assertEquals(3, rdbmsUtils.getRdbmsTables()
-                .orElseThrow(() -> new Exception("No tables were found")).size());
+                .orElseThrow(() -> new RuntimeException("No tables were found")).size());
 
         assertTrue(rdbmsUtils.getRdbmsTable(RDBMS_TABLE_NAME_TO).isPresent());
         assertTrue(rdbmsUtils.getRdbmsTable(RDBMS_TABLE_NAME_FROM).isPresent());
 
         // check junction table
-        assertEquals(1, rdbmsUtils.getRdbmsJunctionTables().
-                orElseThrow(() -> new Exception("No junction tables were found")).size());
+        assertEquals(1, rdbmsUtils.getRdbmsJunctionTables()
+                .orElseThrow(() -> new RuntimeException("No junction tables were found")).size());
 
         assertTrue(rdbmsUtils.getRdbmsJunctionTable(RDBMS_TABLE_NAME_FROM + "#" + ONE_WAY_REFERENCE + " to " + RDBMS_TABLE_NAME_TO).isPresent());
 
 
-        // check if RdbmsForeignKey not exists
+        // check if RdbmsForeignKey not exists unnecessarily
         assertFalse(rdbmsUtils.getRdbmsForeignKey(RDBMS_TABLE_NAME_FROM, ONE_WAY_REFERENCE, false).isPresent());
+    }
+
+    @Test
+    @DisplayName("Test OneWayRelation With One To One Cardinality")
+    public void testOneWayRelationWithOneToOneCardinality() { //This test might be unnecessary though
+        // create annotation
+        EAnnotation eAnnotation = newEAnnotationBuilder()
+                .withSource("http://blackbelt.hu/judo/meta/ExtendedMetadata/entity")
+                .build();
+
+        eAnnotation.getDetails().put("value", "true");
+
+        // create class to
+        final EClass oneWayRelationTo = newEClassBuilder()
+                .withName("OneWayRelationTo")
+                .withEAnnotations(eAnnotation)
+                .build();
+
+
+        // create annotation2
+        EAnnotation eAnnotation2 = newEAnnotationBuilder()
+                .withSource("http://blackbelt.hu/judo/meta/ExtendedMetadata/entity")
+                .build();
+
+        eAnnotation2.getDetails().put("value", "true");
+
+        // create class from
+        final EClass oneWayRelationFrom = newEClassBuilder()
+                .withName("OneWayRelationFrom")
+                .withEAnnotations(eAnnotation2)
+                .withEStructuralFeatures(newEReferenceBuilder()
+                        .withName("oneWayReference")
+                        .withEType(oneWayRelationTo)
+                        .withLowerBound(1)
+                        .withUpperBound(1)
+                        .build())
+                .build();
+
+        // create package
+        final EPackage ePackage = newEPackageBuilder()
+                .withName("TestEpackage")
+                .withEClassifiers(ImmutableList.of(oneWayRelationFrom, oneWayRelationTo))
+                .withNsPrefix("test")
+                .withNsURI("http:///com.example.test.ecore")
+                .build();
+
+        // add content to asm model
+        asmModel.addContent(ePackage);
+
+        executeTransformation("testOneWayRelationWithOneToOneCardinality");
+
+        final String RDBMS_TABLE_NAME_TO = "TestEpackage.OneWayRelationTo";
+        final String RDBMS_TABLE_NAME_FROM = "TestEpackage.OneWayRelationFrom";
+        final String ONE_WAY_REFERENCE = "oneWayReference";
+
+        // check if tables exist
+        assertEquals(2, rdbmsUtils.getRdbmsTables()
+                .orElseThrow(() -> new RuntimeException("No tables were found")).size());
+
+        // check if RdbmsForeignKey is valid
+        assertEquals(rdbmsUtils.getRdbmsTable(RDBMS_TABLE_NAME_TO)
+                        .orElseThrow(() -> new RuntimeException("No " + RDBMS_TABLE_NAME_TO + " were found")).getPrimaryKey(),
+                rdbmsUtils.getRdbmsForeignKey(RDBMS_TABLE_NAME_FROM, ONE_WAY_REFERENCE, false)
+                        .orElseThrow(() -> new RuntimeException("No " + RDBMS_TABLE_NAME_FROM + "#" + ONE_WAY_REFERENCE + " were found"))
+                        .getReferenceKey());
+    }
+
+    //////////////////////////////////////////////////
+    /////////////// TWO WAY RELATIONS ////////////////
+
+    @Test
+    @Disabled //FIXME
+    @DisplayName("Test TwoWayRelation With Null To One Cardinalities")
+    public void testTwoWayRelationWithNullToOneCardinalities() {
+        // create annotations
+        final EAnnotation eAnnotation = newEAnnotationBuilder()
+                .withSource("http://blackbelt.hu/judo/meta/ExtendedMetadata/entity")
+                .build();
+
+        eAnnotation.getDetails().put("value", "true");
+
+        final EAnnotation eAnnotation2 = newEAnnotationBuilder()
+                .withSource("http://blackbelt.hu/judo/meta/ExtendedMetadata/entity")
+                .build();
+
+        eAnnotation2.getDetails().put("value", "true");
+
+        // create classes
+        final EClass twoWayRelation1 = newEClassBuilder()
+                .withName("TwoWayRelation1")
+                .withEAnnotations(eAnnotation)
+                .build();
+
+        final EClass twoWayRelation2 = newEClassBuilder()
+                .withName("TwoWayRelation2")
+                .withEAnnotations(eAnnotation2)
+                .build();
+
+        // create references
+        final EReference twoWayReference1 = newEReferenceBuilder()
+                .withName("twoWayReference1")
+                .withLowerBound(0)
+                .withUpperBound(1)
+                .withEType(twoWayRelation1)
+                .build();
+
+        final EReference twoWayReference2 = newEReferenceBuilder()
+                .withName("twoWayReference2")
+                .withLowerBound(0)
+                .withUpperBound(1)
+                .withEType(twoWayRelation2)
+                .build();
+
+        // add opposites
+        twoWayReference1.setEOpposite(twoWayReference2);
+        twoWayReference2.setEOpposite(twoWayReference1);
+
+        // add references
+        twoWayRelation1.getEStructuralFeatures().add(twoWayReference2);
+        twoWayRelation2.getEStructuralFeatures().add(twoWayReference1);
+
+        // create package
+        final EPackage ePackage = newEPackageBuilder()
+                .withName("TestEpackage")
+                .withEClassifiers(ImmutableList.of(twoWayRelation1, twoWayRelation2))
+                .withNsPrefix("test")
+                .withNsURI("http:///com.example.test.ecore")
+                .build();
+
+        // add content to asm model
+        asmModel.addContent(ePackage);
+
+        executeTransformation("testTwoWayRelationWithNullToOneCardinalities");
+
+        final String RDBMS_TABLE_NAME_1 = "TestEpackage.TwoWayRelation1";
+        final String RDBMS_TABLE_NAME_2 = "TestEpackage.TwoWayRelation2";
+        final String TWO_WAY_REFERENCE1 = "twoWayReference1";
+        final String TWO_WAY_REFERENCE2 = "twoWayReference2";
+
+        // check if tables exist
+        assertEquals(2, rdbmsUtils.getRdbmsTables()
+                .orElseThrow(() -> new RuntimeException("No tables were found")).size());
+
+        //FIXME: attribures of asm model get consumed during transformation
+        assertEquals(3, rdbmsUtils.getRdbmsFields(RDBMS_TABLE_NAME_1)
+                .orElseThrow(() -> new RuntimeException(RDBMS_TABLE_NAME_1 + " table not found"))
+                .size()); //+2 id and type
+
+        assertEquals(3, rdbmsUtils.getRdbmsFields(RDBMS_TABLE_NAME_2)
+                .orElseThrow(() -> new RuntimeException(RDBMS_TABLE_NAME_2 + " table not found"))
+                .size());  //+2 id and type
+
+        //FIXME: attribures of asm model get consumed during transformation
+        assertEquals(rdbmsUtils.getRdbmsTable(RDBMS_TABLE_NAME_2)
+                        .orElseThrow(() -> new RuntimeException(RDBMS_TABLE_NAME_2 + " table not found"))
+                        .getPrimaryKey(),
+                rdbmsUtils.getRdbmsForeignKey(RDBMS_TABLE_NAME_1, TWO_WAY_REFERENCE2, false)
+                        .orElseThrow(() -> new RuntimeException(TWO_WAY_REFERENCE2 + " attribute not found"))
+                        .getReferenceKey());
+
+        assertEquals(rdbmsUtils.getRdbmsTable(RDBMS_TABLE_NAME_1)
+                        .orElseThrow(() -> new RuntimeException(RDBMS_TABLE_NAME_1 + " table not found"))
+                        .getPrimaryKey(),
+                rdbmsUtils.getRdbmsForeignKey(RDBMS_TABLE_NAME_2, TWO_WAY_REFERENCE1, false)
+                        .orElseThrow(() -> new RuntimeException(TWO_WAY_REFERENCE1 + " attribute not found"))
+                        .getReferenceKey());
     }
 
 }
