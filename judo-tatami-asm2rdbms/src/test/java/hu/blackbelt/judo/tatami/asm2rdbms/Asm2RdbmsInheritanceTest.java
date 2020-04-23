bbf421fec6ffa2a05c7ddc4fa67b0e197e6ce1bd
Gelei Bence <bence.gelei@blackbelt.hu>
2020-04-23 09:52:40 +0200

    JNG-1242 testInheritanceWithTwoParents added

--- judo-tatami-asm2rdbms/src/test/java/hu/blackbelt/judo/tatami/asm2rdbms/Asm2RdbmsInheritanceTest.java
+++ judo-tatami-asm2rdbms/src/test/java/hu/blackbelt/judo/tatami/asm2rdbms/Asm2RdbmsInheritanceTest.java
@@ -6,6 +6,8 @@ import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcorePackage;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.eclipse.emf.ecore.util.builder.EcoreBuilders.*;
@@ -13,7 +15,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Asm2RdbmsInheritanceTest extends Asm2RdbmsMappingTestBase {
+
     @Test
+    @DisplayName("Test Basic Inheritance")
     public void testBasicInheritance() {
         final EcorePackage ecore = EcorePackage.eINSTANCE;
 
@@ -104,5 +108,92 @@ public class Asm2RdbmsInheritanceTest extends Asm2RdbmsMappingTestBase {
         assertEquals(rdbmsTable_fruit.getPrimaryKey(),
                 rdbmsTable_apple.getParents().get(0).getPrimaryKey());
     }
+
+    @Disabled
+    @Test
+    @DisplayName("Test Inheritance With Two Parents")
+    public void testInheritanceWithTwoParents() {
+        final EcorePackage ecore = EcorePackage.eINSTANCE;
+
+        // create annotation
+        EAnnotation eAnnotation = newEAnnotationBuilder()
+                .withSource("http://blackbelt.hu/judo/meta/ExtendedMetadata/entity")
+                .build();
+
+        eAnnotation.getDetails().put("value", "true");
+
+        // create annotation1
+        EAnnotation eAnnotation1 = newEAnnotationBuilder()
+                .withSource("http://blackbelt.hu/judo/meta/ExtendedMetadata/entity")
+                .build();
+
+        eAnnotation.getDetails().put("value", "true");
+
+        // create annotation2
+        EAnnotation eAnnotation2 = newEAnnotationBuilder()
+                .withSource("http://blackbelt.hu/judo/meta/ExtendedMetadata/entity")
+                .build();
+
+        eAnnotation.getDetails().put("value", "true");
+
+        // create eclass vegetable
+        EClass vegetable = newEClassBuilder()
+                .withName("vegetables")
+                .withEAnnotations(eAnnotation)
+                .withEStructuralFeatures(
+                        newEAttributeBuilder()
+                                .withName("isVegetable")
+                                .withEType(ecore.getEBoolean())
+                                .build()
+                )
+                .build();
+
+        // create eclass fruit
+        EClass fruit = newEClassBuilder()
+                .withName("fruits")
+                .withEAnnotations(eAnnotation1)
+                .withEStructuralFeatures(
+                        newEAttributeBuilder()
+                                .withName("isFruit")
+                                .withEType(ecore.getEBoolean())
+                                .build()
+                )
+                .build();
+
+        // create eclass tomato
+        EClass tomato = newEClassBuilder()
+                .withName("tomato")
+                .withEAnnotations(eAnnotation2)
+                .withEStructuralFeatures(
+                        newEAttributeBuilder()
+                                .withName("tomato_type")
+                                .withEType(ecore.getEString())
+                                .build()
+                )
+                .withESuperTypes(ImmutableList.of(vegetable, fruit))
+                .build();
+
+        // create package
+        final EPackage ePackage = newEPackageBuilder()
+                .withName("TestEpackage")
+                .withEClassifiers(ImmutableList.of(vegetable, fruit, tomato))
+                .withNsPrefix("test")
+                .withNsURI("http:///com.example.test.ecore")
+                .build();
+
+        // add content to asm model
+        asmModel.addContent(ePackage);
+
+        executeTransformation("testInheritanceWithTwoParents");
+
+        // ASSERTION - check if correct number of tables were created
+        assertEquals(3, rdbmsUtils.getRdbmsTables()
+                .orElseThrow(() -> new RuntimeException("No tables found"))
+                .size());
+
+        //TODO: test fails
+
+    }
+
 
 }
