2a035aca75b2e8f429a26a8339d61af6bcab3f54
fruska <fruzsifruzsifruzsi@gmail.com>
2020-09-09 14:09:22 +0200

    JNG-1406: create organize application method that organizes page containers and clones them if necessary

--- judo-tatami-esm2ui/src/test/java/hu/blackbelt/judo/tatami/esm2ui/Northwind2UiApplicationTest.java
+++ judo-tatami-esm2ui/src/test/java/hu/blackbelt/judo/tatami/esm2ui/Northwind2UiApplicationTest.java
@@ -4,8 +4,13 @@ import hu.blackbelt.epsilon.runtime.execution.api.Log;
 import hu.blackbelt.epsilon.runtime.execution.impl.Slf4jLog;
 import hu.blackbelt.judo.meta.esm.runtime.EsmModel;
 import hu.blackbelt.judo.meta.ui.Application;
+import hu.blackbelt.judo.meta.ui.Container;
 import hu.blackbelt.judo.meta.ui.NavigationController;
+import hu.blackbelt.judo.meta.ui.Tab;
+import hu.blackbelt.judo.meta.ui.TabController;
+import hu.blackbelt.judo.meta.ui.VisualElement;
 import hu.blackbelt.judo.meta.ui.runtime.UiModel;
+import hu.blackbelt.judo.meta.ui.runtime.UiUtils;
 import hu.blackbelt.model.northwind.esm.NorthwindEsmModel;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.emf.common.util.URI;
@@ -83,6 +88,14 @@ public class Northwind2UiApplicationTest {
         final Optional<Application> application = allUi(Application.class)
                 .findAny();
         assertTrue(application.isPresent());
+        
+        application.get().getPages().stream()
+    	.forEach(p -> 
+    		p.getContainers().stream()
+    			.forEach(c -> 
+    				slf4jlog.debug("Printing UI model page " + testName + "::" + p.getName() + ": \n" + printElement(c))
+    			)
+    	);
 
         final Optional<NavigationController> navigationController = allUi(NavigationController.class)
                 .findAny();
@@ -103,6 +116,57 @@ public class Northwind2UiApplicationTest {
 
     private <T> Stream<T> allUi(final Class<T> clazz) {
         return allUi().filter(e -> clazz.isAssignableFrom(e.getClass())).map(e -> (T) e);
+    }
+    
+    private static String printElement(VisualElement element) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(print(element,0,false));
+        return sb.toString();
+    }
+      
+    private static String print(VisualElement element, int level, boolean isTab) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; ++i) sb.append("  ");
+        if (isTab) sb.append("TAB: ");
+        sb.append(element.getName());
+        
+        if(UiUtils.isHorizontalFlex(element)) {
+          sb.append("---HORIZONTAL---");
+          int sumCol = UiUtils.getSumOfCols((Container) element);
+          int maxRow = UiUtils.getMaxOfRows((Container) element);
+          sb.append(maxRow + " rows in " + element.getRow());
+          if (maxRow <= element.getRow()) sb.append(" ***FITS*** //////// ");
+          else sb.append(" ===WRONG=== //////// ");
+          sb.append(sumCol + " cols in " + element.getCol());
+          if (sumCol <= element.getCol()) sb.append(" ***FITS*** ");
+          else sb.append(" ===WRONG=== ");
+        }
+        else if(UiUtils.isVerticalFlex(element)) {
+          sb.append("|||VERTICAL||| ");
+          int sumRow = UiUtils.getSumOfRows((Container) element);
+          int maxCol = UiUtils.getMaxOfCols((Container) element);
+
+          sb.append(sumRow + " rows in " + element.getRow());
+          if (sumRow <= element.getRow()) sb.append(" ***FITS*** //////// ");
+          else sb.append(" ===WRONG=== //////// ");
+          
+          sb.append(maxCol + " cols in " + element.getCol());
+          if (maxCol <= element.getCol()) sb.append(" ***FITS*** ");
+          else sb.append(" ===WRONG=== ");
+        } else {
+          sb.append(" (" + element.getRow() + "*" + element.getCol() + ")");
+        }
+        
+        if (element instanceof Container) {
+          sb.append("\n");
+          for (VisualElement c : ((Container) element).getChildren()) sb.append(print(c,level+1,false));
+        } else if (element instanceof TabController) {
+        	sb.append("\n");
+            for (Tab c : ((TabController) element).getTabs()) sb.append(print(c.getElement(),level+1,true));
+        } else {
+          sb.append("\n");
+        }
+        return sb.toString();
     }
 
 }
