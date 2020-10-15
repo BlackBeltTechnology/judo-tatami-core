f6c67e88627f0573ea29be32597cc777b61b20c0
balintbokor <balint.bokor@blackbelt.hu>
2020-10-15 12:51:49 +0200

    JNG-1406 Add missing components, fix disabled constant value and import

--- judo-tatami-ui2client/src/main/resources/templates/flutter/lib/utilities/constants.dart
+++ judo-tatami-ui2client/src/main/resources/templates/flutter/lib/utilities/constants.dart
@@ -2,6 +2,7 @@ part of judo.utility;
 
 const Color kPrimaryColor = Color(0xFFff6f00);
 const Color kSecondaryColor = Color(0xFF0288d1);
+const Color kDisabledColor = Color(0x31000000);
 
 const double kJudoCardMargin = 4.0;
 
