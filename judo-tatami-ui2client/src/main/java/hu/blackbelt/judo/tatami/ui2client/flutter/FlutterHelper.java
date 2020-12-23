b511a5c6b1d86c61d56719cf653a479ca72aa7fa
Roland Knobloch <knobico@gmail.com>
2020-12-23 16:45:48 +0100

    JNG-2165 Fix validate issue in containment create

--- judo-tatami-ui2client/src/main/java/hu/blackbelt/judo/tatami/ui2client/flutter/FlutterHelper.java
+++ judo-tatami-ui2client/src/main/java/hu/blackbelt/judo/tatami/ui2client/flutter/FlutterHelper.java
@@ -40,6 +40,7 @@ public class FlutterHelper {
         context.registerFunction("dartType", FlutterHelper.class.getDeclaredMethod("dartType", new Class[]{DataType.class}));
         context.registerFunction("isTransientAttribute", FlutterHelper.class.getDeclaredMethod("isTransientAttribute", new Class[]{AttributeType.class}));
         context.registerFunction("multiplyCol", FlutterHelper.class.getDeclaredMethod("multiplyCol", new Class[]{Double.class}));
+        context.registerFunction("validatableFlagNeed", FlutterHelper.class.getDeclaredMethod("validatableFlagNeed", new Class[]{RelationType.class}));
         context.registerFunction("isEnumType", FlutterHelper.class.getDeclaredMethod("isEnumType", new Class[]{DataType.class}));
         context.registerFunction("isInputWidgetMapNeed", FlutterHelper.class.getDeclaredMethod("isInputWidgetMapNeed", new Class[]{PageDefinition.class}));
         context.registerFunction("getInputWidgets", FlutterHelper.class.getDeclaredMethod("getInputWidgets", new Class[]{Container.class}));
@@ -229,6 +230,14 @@ public class FlutterHelper {
         } else {
             return "String";
         }
+    }
+
+    public static boolean validatableFlagNeed (RelationType relationType) {
+        return relationType.getIsRelationKindComposition() || relationType.getIsRelationKindAggregation() || relationType.getTarget().getRelations()
+                .stream()
+                .anyMatch(
+                        relationTypeElement -> relationTypeElement.getIsRelationKindAggregation() || relationTypeElement.getIsRelationKindComposition()
+                );
     }
 
     public static boolean isEnumType (DataType type) {
