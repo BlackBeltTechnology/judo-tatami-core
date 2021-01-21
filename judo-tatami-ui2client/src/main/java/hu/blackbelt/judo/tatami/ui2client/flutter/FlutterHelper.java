631c61676a1e15757a70eb6abb521033f8c36096
balintbokor <balint.bokor@blackbelt.hu>
2021-01-21 17:29:59 +0100

    JNG-2277 Boolean value must be initialized to false in model store

--- judo-tatami-ui2client/src/main/java/hu/blackbelt/judo/tatami/ui2client/flutter/FlutterHelper.java
+++ judo-tatami-ui2client/src/main/java/hu/blackbelt/judo/tatami/ui2client/flutter/FlutterHelper.java
@@ -43,6 +43,7 @@ public class FlutterHelper {
         context.registerFunction("validatableFlagNeed", FlutterHelper.class.getDeclaredMethod("validatableFlagNeed", new Class[]{RelationType.class}));
         context.registerFunction("isEnumType", FlutterHelper.class.getDeclaredMethod("isEnumType", new Class[]{DataType.class}));
         context.registerFunction("isTimestampType", FlutterHelper.class.getDeclaredMethod("isTimestampType", new Class[]{DataType.class}));
+        context.registerFunction("isBooleanDataType", FlutterHelper.class.getDeclaredMethod("isBooleanDataType", new Class[]{DataType.class}));
         context.registerFunction("isDateType", FlutterHelper.class.getDeclaredMethod("isDateType", new Class[]{DataType.class}));
         context.registerFunction("isInputWidgetMapNeed", FlutterHelper.class.getDeclaredMethod("isInputWidgetMapNeed", new Class[]{PageDefinition.class}));
         context.registerFunction("getInputWidgets", FlutterHelper.class.getDeclaredMethod("getInputWidgets", new Class[]{Container.class}));
@@ -52,6 +53,9 @@ public class FlutterHelper {
         handlebars.registerHelpers(FlutterHelper.class);
     }
 
+    public static boolean isBooleanDataType (DataType dataType) {
+        return dataType instanceof BooleanType;
+    }
     public static boolean isTransientAttribute (AttributeType attributeType) {
         return MemberType.TRANSIENT == attributeType.getMemberType();
     }
