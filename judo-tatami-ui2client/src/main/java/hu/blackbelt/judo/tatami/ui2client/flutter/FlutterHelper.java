d329497b2a3629759e0cd8f49fad42eda4087205
Robert Csakany <csakany.robert@gmail.com>
2021-01-24 13:55:09 +0100

    JNG-2314 JUDO Link added

--- judo-tatami-ui2client/src/main/java/hu/blackbelt/judo/tatami/ui2client/flutter/FlutterHelper.java
+++ judo-tatami-ui2client/src/main/java/hu/blackbelt/judo/tatami/ui2client/flutter/FlutterHelper.java
@@ -43,9 +43,9 @@ public class FlutterHelper {
         context.registerFunction("validatableFlagNeed", FlutterHelper.class.getDeclaredMethod("validatableFlagNeed", new Class[]{RelationType.class}));
         context.registerFunction("isEnumType", FlutterHelper.class.getDeclaredMethod("isEnumType", new Class[]{DataType.class}));
         context.registerFunction("isTimestampType", FlutterHelper.class.getDeclaredMethod("isTimestampType", new Class[]{DataType.class}));
-        context.registerFunction("isBooleanDataType", FlutterHelper.class.getDeclaredMethod("isBooleanDataType", new Class[]{DataType.class}));
         context.registerFunction("isDateType", FlutterHelper.class.getDeclaredMethod("isDateType", new Class[]{DataType.class}));
         context.registerFunction("isInputWidgetMapNeed", FlutterHelper.class.getDeclaredMethod("isInputWidgetMapNeed", new Class[]{PageDefinition.class}));
+        context.registerFunction("isValidateHere", FlutterHelper.class.getDeclaredMethod("isValidateHere", new Class[]{PageDefinition.class}));
         context.registerFunction("getInputWidgets", FlutterHelper.class.getDeclaredMethod("getInputWidgets", new Class[]{Container.class}));
     }
 
@@ -53,14 +53,11 @@ public class FlutterHelper {
         handlebars.registerHelpers(FlutterHelper.class);
     }
 
-    public static boolean isBooleanDataType (DataType dataType) {
-        return dataType instanceof BooleanType;
-    }
     public static boolean isTransientAttribute (AttributeType attributeType) {
         return MemberType.TRANSIENT == attributeType.getMemberType();
     }
 
-   public static String mainAxisSize(Flex flex) {
+    public static String mainAxisSize(Flex flex) {
         return flex.getMainAxisSize().getLiteral().toLowerCase();
     }
 
@@ -280,6 +277,10 @@ public class FlutterHelper {
 
         }
         return inputList;
+    }
+
+    public static boolean isValidateHere(PageDefinition page){
+        return !page.getIsPageTypeUpdate() && !page.getIsPageTypeView() && !page.getIsPageTypeOperationOutput();
     }
 
     public static void getInputWidgetsFromContainers(Container container, List<VisualElement> inputList) {
