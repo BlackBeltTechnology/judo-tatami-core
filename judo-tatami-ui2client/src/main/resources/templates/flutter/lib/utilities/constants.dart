d9f9f974523160bbaed5f925324bf9ef973b0e43
Robert Csakany <csakany.robert@gmail.com>
2021-01-30 21:28:27 +0100

    JNG-2383 The RowSelect is navigate to View of entry, eye removed

--- judo-tatami-ui2client/src/main/resources/templates/flutter/lib/utilities/constants.dart
+++ judo-tatami-ui2client/src/main/resources/templates/flutter/lib/utilities/constants.dart
@@ -2,12 +2,3 @@ part of judo.utility;
 
 const bool isNavigateToSettingsPage = bool.fromEnvironment("TO_SETTINGS_PAGE");
 
-const Color kPrimaryColor = Color(0xFFff6f00);
-const Color kSecondaryColor = Color(0xFF0288d1);
-const Color kDisabledColor = Color(0x31000000);
-
-const double kJudoCardMargin = 4.0;
-
-const double kJudoCardPadding = 32.0;
-
-const double kJudoHeight = 56.0;
